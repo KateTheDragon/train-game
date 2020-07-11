0620a2ab61f7e923af9891f05357761ac8e9c455
Kate Peterson <petersonkatec@gmail.com>
2020-07-11 15:53:13 -0230

    The train moves and the display crops right

---
+++ train game/Assets/TrainMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainMovement : MonoBehaviour
+{
+    float speed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(-10, 0.5f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+    }
+}
